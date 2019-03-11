0ef47b50dccf4119c04c55ec96c61b8db10f57d6
Johnson Zhong <zhsh@umich.edu>
2019-03-11 11:42:35 -0400

    Solve problem

--- README.md
+++ README.md
@@ -19,3 +19,45 @@ npm run start
 ```
 
 And use console to evaluate the expressions on the `lib` library.
+
+## Solution
+
+The current webpack configuration solves this issue by explicitly keeping function and class names:
+
+```
+    optimization: {
+        minimizer: [
+            new UglifyJSPlugin({
+                cache        : true,
+                parallel     : true,
+                uglifyOptions: {
+                    compress: true,
+                    ecma    : 6,
+                    keep_fnames: true,
+                    keep_classnames: true,
+                },
+                sourceMap    : true
+            })
+
+        ]
+    },
+```
+
+Although UglifyJS only works on ES5, so we have to pull in Babel as well:
+
+```
+    module      : {
+        rules: [
+            {
+                test   : /\.js$/,
+                include: [/src/],
+                use    : {
+                    loader : 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+        ]
+    },
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const CleanWebpackPlugin = require('clean-webpack-plugin');
 
@@ -24,5 +26,37 @@ module.exports = {
         path         : path.resolve(__dirname, 'dist'),
         library      : 'lib',
         libraryTarget: 'umd',
+    },
+    // solves the issue by keeping function and class names
+    // although Babel is required here because the source is ES6 and UglifyJS only deals with ES5
+    optimization: {
+        minimizer: [
+            new UglifyJSPlugin({
+                cache        : true,
+                parallel     : true,
+                uglifyOptions: {
+                    compress: true,
+                    ecma    : 6,
+                    keep_fnames: true,
+                    keep_classnames: true,
+                },
+                sourceMap    : true
+            })
+
+        ]
+    },
+    module      : {
+        rules: [
+            {
+                test   : /\.js$/,
+                include: [/src/],
+                use    : {
+                    loader : 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+        ]
     },
 };
